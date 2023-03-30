93b0043f21a039f4313d7cd2a970c65ba5efe3e6
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-03-30 16:57:40 +0300

    Add styles to card

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -3,19 +3,35 @@ import styles from './UserCard.module.scss'
 const UserCard = () => {
   return (
     <section className={styles.userCard}>
-      <div>
+      <div className={styles.card}>
         <div className={styles.registration}>
           <p className={styles.name}>The Octocat</p>
           <p className={styles.joined}>Joined 25 Jan 2011</p>
         </div>
-        <p>@octocat</p>
+        <p className={styles.tag}>@octocat</p>
 
-        <p>This profile has no bio</p>
+        <p className={styles.bio}>This profile has no bio</p>
 
-        <p>Repos</p>
-        <p>Followers</p>
-        <p>Following</p>
-        <p>Joined</p>
+        <div className={styles.info}>
+          <div className={styles.info_about}>
+            <p className={styles.info_num}>Repos</p>
+            <p className={styles.count}>8</p>
+          </div>
+          <div className={styles.info_about}>
+            <p className={styles.info_num}>Followers</p>
+            <p className={styles.count}>3938</p>
+          </div>
+          <div className={styles.info_about}>
+            <p className={styles.info_num}>Following</p>
+            <p className={styles.count}>9</p>
+          </div>
+        </div>
+        <div>
+          <p>San Francisco</p>
+          <p>https://github.blog</p>
+          <p>@github</p>
+          <p>Not Available</p>
+        </div>
       </div>
     </section>
   )
