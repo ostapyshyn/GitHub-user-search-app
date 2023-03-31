8c5eeeded77826fe7fd17c78a3b1191af0f8ff83
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-03-31 10:32:57 +0300

    Add header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
+import styles from './Header.module.scss'
+
 const Header = () => {
   return (
-    <section>
-      <div>devfinder</div>
+    <section className={styles.header}>
+      <h1 className={styles.logo}>devfinder</h1>
       <p>dark</p>
     </section>
   )
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,17 @@
 import styles from './SearchBar.module.scss'
+import search from '../../assets/icon-search.svg'
 
 const SearchBar = () => {
   return (
     <section className={styles.searchBar}>
       <div>
-        <input type="text" placeholder="Search GitHub username…" />
-        <button>search</button>
+        <form>
+          <img src={search} alt="search" />
+          <input type="text" placeholder="Search GitHub username…" />
+          <button type="button" className={styles.search_button}>
+            Search
+          </button>
+        </form>
       </div>
     </section>
   )
