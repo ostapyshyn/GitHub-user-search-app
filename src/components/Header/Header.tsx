698d19328149cd5ba4677b13d489ead983c82262
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-04-04 10:06:26 +0300

    Fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import styles from './Header.module.scss'
+import sun from '../../assets/icon-sun.svg'
 
 const Header = () => {
   return (
     <section className={styles.header}>
       <h1 className={styles.logo}>devfinder</h1>
-      <p>dark</p>
+
+      <button className={styles.switcher}>
+        <span>LIGHT</span>
+        <img src={sun} alt="sun" />
+      </button>
     </section>
   )
 }
