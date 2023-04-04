ff7bc8e6c1266675890305e50cffbe96552644a7
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-04-04 17:21:55 +0300

    Fix search bar styles and hovering

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,30 @@
 import styles from './SearchBar.module.scss'
 import search from '../../assets/icon-search.svg'
+import { useEffect, useRef } from 'react'
 
 const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+
   return (
     <section className={styles.searchBar}>
-      <div>
-        <form>
-          <img src={search} alt="search" />
-          <input type="text" placeholder="Search GitHub username…" />
-          <button type="button" className={styles.search_button}>
-            Search
-          </button>
-        </form>
-      </div>
+      <img src={search} alt="search" className={styles.search} />
+      <form>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search GitHub username…"
+          className={styles.search_input}
+        />
+        <button type="button" className={styles.search_button}>
+          Search
+        </button>
+      </form>
     </section>
   )
 }
